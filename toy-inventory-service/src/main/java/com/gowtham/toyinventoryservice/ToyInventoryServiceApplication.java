a689b3011181c0b5e62d9ebd6c5c2e22b56fc26b
Gowtham Paramasivam <gowthamparamasivam66@gmail.com>
2021-05-10 18:05:25 +0530

    toy-story-inventory service has been added

---
+++ toy-inventory-service/src/main/java/com/gowtham/toyinventoryservice/ToyInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gowtham.toyinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyInventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyInventoryServiceApplication.class, args);
+    }
+
+}
---
+++ toy-inventory-service/src/test/java/com/gowtham/toyinventoryservice/ToyInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gowtham.toyinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyInventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
