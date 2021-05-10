b08afa87138e7228c0d4dec884c0b8645ade023d
Gowtham Paramasivam <gowthamparamasivam66@gmail.com>
2021-05-10 13:01:41 +0530

    docker-compose-files and eureka server with basic authentication

---
+++ toy-store-eureka-server/src/test/java/com/gowtham/toystoreeurekaserver/ToyStoreEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gowtham.toystoreeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyStoreEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
